37a62bc4426016431cacc58f7f631d2de47989e4
Thanh Hung <hungpt.se@gmail.com>
2019-03-25 01:03:58 +0700

    Deplpoy Heroku

---
+++ build/precache-manifest.c028ffff13fdf71b067fb99471675942.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0a3f5696dd9d8b1290c",
+    "url": "/static/css/main.a3cc9d9f.chunk.css"
+  },
+  {
+    "revision": "d0a3f5696dd9d8b1290c",
+    "url": "/static/js/main.d0a3f569.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b46514b7468551718e89",
+    "url": "/static/css/2.fcf0dc54.chunk.css"
+  },
+  {
+    "revision": "b46514b7468551718e89",
+    "url": "/static/js/2.b46514b7.chunk.js"
+  },
+  {
+    "revision": "8ab12c496de37bfd940ae032c64aafa4",
+    "url": "/static/media/react.8ab12c49.png"
+  },
+  {
+    "revision": "0696aa54c5b646340083d7051dc5f5a7",
+    "url": "/static/media/redux.0696aa54.png"
+  },
+  {
+    "revision": "6aa17d6911e711c03fb4c8fb0202c21f",
+    "url": "/static/media/asp.6aa17d69.png"
+  },
+  {
+    "revision": "e642c7556d83be574ae3929c34ead074",
+    "url": "/static/media/firebase.e642c755.png"
+  },
+  {
+    "revision": "7237b6a2c8b2c1fd23a5c3759160d81a",
+    "url": "/static/media/shopping-bag.7237b6a2.svg"
+  },
+  {
+    "revision": "bc4634b50b5ff9d94c9ac8b8e90e00cc",
+    "url": "/static/media/close.bc4634b5.svg"
+  },
+  {
+    "revision": "09574f6cb4ba9b4a8bcd177197789c25",
+    "url": "/static/media/default-product.09574f6c.jpg"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "f7b5942a6b014d29255f7b4e7712d531",
+    "url": "/index.html"
+  }
+];
